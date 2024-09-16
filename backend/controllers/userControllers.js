98906d1f97ab0b1a1957a84ea7ab9516321ba2bf
LokeshSr03 <lokeshsuthar0123@gmail.com>
2024-09-16 12:04:15 +0530

    getprofile and updateprofile done

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -15,6 +15,7 @@ const register = asyncHandler(async (req, res) => {
 
   if (user) {
     res.status(200).json({
+      _id: user._id,
       username: user.username,
       email: user.email,
       address: user.address,
@@ -33,6 +34,7 @@ const login = asyncHandler(async (req, res) => {
 
   if (user && (await user.matchPassword(password))) {
     res.json({
+      _id: user._id,
       username: user.username,
       email: user.email,
       address: user.address,
@@ -43,4 +45,44 @@ const login = asyncHandler(async (req, res) => {
   }
 });
 
-export { register, login };
+const getProfile = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  const user = await User.findById(id);
+
+  if (user) {
+    res.json({
+      username: user.username,
+      email: user.email,
+      address: user.address,
+    });
+  } else {
+    throw new Error("User Data not Found");
+  }
+});
+
+const updateProfile = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+  const { username, email, password, address } = req.body;
+
+  const user = await User.findById(id);
+
+  if (user) {
+    user.username = username || user.username;
+    user.email = email || user.email;
+    user.password = password || user.password;
+    user.address = address || user.address;
+
+    const updatedUser = await user.save();
+
+    res.json({
+      username: updatedUser.username,
+      email: updatedUser.email,
+      address: updatedUser.address,
+    });
+  } else {
+    throw new Error("User not found");
+  }
+});
+
+export { register, login, getProfile, updateProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { login, register } from "../controllers/userControllers.js";
+import { protect } from "../middlewares/authmiddleware.js";
+import {
+  getProfile,
+  login,
+  register,
+  updateProfile,
+} from "../controllers/userControllers.js";
 
 const router = express.Router();
 
-router.route("/:id");
+router.route("/:id").get(protect, getProfile).put(protect, updateProfile);
 router.route("/register").post(register);
 router.route("/login").post(login);
 

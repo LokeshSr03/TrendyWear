83a2d5f367d02f5ababb2849188467f474a163a6
LokeshSr03 <lokeshsuthar0123@gmail.com>
2024-09-17 16:33:17 +0530

    changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,16 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { MenuIcon, XIcon, ChevronDownIcon } from "@heroicons/react/outline"; // Menu and Close icons from HeroIcons
 import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { FiShoppingCart } from "react-icons/fi"; // Import the cart icon
+import { useSelector } from "react-redux";
 
 const Header = () => {
   const [menuOpen, setMenuOpen] = useState(false); // State for menu toggle
+
+  const userLogin = useSelector((state) => state.userLogin);
+
+  const { userInfo } = userLogin;
 
   const toggleMenu = () => {
     setMenuOpen(!menuOpen); // Toggle menu state
@@ -72,36 +78,46 @@ const Header = () => {
           <Link to="/services" className="text-white">
             Services
           </Link>
-          <Link to="/contact" className="text-white">
-            Contact
-          </Link>
-          <Menu as="div" className="relative inline-block text-left">
-            <div>
-              <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                Options
-                <ChevronDownIcon
-                  aria-hidden="true"
-                  className="-mr-1 h-5 w-5 text-gray-400"
-                />
-              </MenuButton>
-            </div>
 
-            <MenuItems
-              transition
-              className="absolute right-0 z-10 mt-3 w-32 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none data-[closed]:scale-95 data-[closed]:transform data-[closed]:opacity-0 data-[enter]:duration-100 data-[leave]:duration-75 data-[enter]:ease-out data-[leave]:ease-in"
-            >
-              <div className="py-1">
-                <MenuItem>
-                  <Link
-                    to="/profile"
-                    className="block px-4 py-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900"
-                  >
-                    Profile
-                  </Link>
-                </MenuItem>
+          {/* Cart Icon Link */}
+          <Link to="/cart" className="text-white">
+            <FiShoppingCart className="h-6 w-6" />
+          </Link>
+
+          {/* Dropdown Menu */}
+          {userInfo ? (
+            <Menu as="div" className="relative inline-block text-left">
+              <div>
+                <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
+                  {userInfo && userInfo.username}
+                  <ChevronDownIcon
+                    aria-hidden="true"
+                    className="-mr-1 h-5 w-5 text-gray-400"
+                  />
+                </MenuButton>
               </div>
-            </MenuItems>
-          </Menu>
+
+              <MenuItems
+                transition
+                className="absolute right-0 z-10 mt-3 w-32 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 transition focus:outline-none"
+              >
+                <div className="py-1">
+                  <MenuItem>
+                    <Link
+                      to="/profile"
+                      className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
+                    >
+                      Profile
+                    </Link>
+                  </MenuItem>
+                </div>
+              </MenuItems>
+            </Menu>
+          ) : (
+            <Link to="/login" className="text-white">
+              Login
+            </Link>
+          )}
         </div>
       </nav>
     </header>

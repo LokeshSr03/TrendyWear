42b2f1fc98bccec59ff816e3bf90d6b9b86d7e54
LokeshSr03 <lokeshsuthar0123@gmail.com>
2024-09-18 15:25:35 +0530

    changes

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -124,7 +124,7 @@ const Header = () => {
               <Menu as="div" className="relative inline-block text-left">
                 <div>
                   <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
-                    {userInfo && userInfo.username}
+                    Manage
                     <ChevronDownIcon
                       aria-hidden="true"
                       className="-mr-1 h-5 w-5 text-gray-400"
@@ -139,17 +139,17 @@ const Header = () => {
                   <div className="py-1">
                     <MenuItem>
                       <Link
-                        to="/profile"
+                        to="/product"
                         className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 hover:text-gray-900"
                       >
-                        Profile
+                        Create Product
                       </Link>
                     </MenuItem>
                   </div>
                 </MenuItems>
               </Menu>
             </>
-          ) : !userInfo.isAdmin ? (
+          ) : userInfo && !userInfo.isAdmin ? (
             <Menu as="div" className="relative inline-block text-left">
               <div>
                 <MenuButton className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">
